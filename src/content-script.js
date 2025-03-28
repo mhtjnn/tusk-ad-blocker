e17deb9e27e88be2a28ef9b35a116ab53334cf5b
Abhimanyu-13 <abhimanyu@genboot.com>
2025-03-28 19:34:36 +0530

    Update content-script.js

--- src/content-script.js
+++ src/content-script.js
@@ -4,15 +4,6 @@ messageDiv.id = 'tusk-privacy-installed';
 
 // Style the div (optional, remove if no styling is needed)
 messageDiv.style.position = 'fixed';
-messageDiv.style.bottom = '0';
-// messageDiv.style.width = '100%';
-messageDiv.style.backgroundColor = '#cdced0';
-messageDiv.style.color = '#dc3545';
-messageDiv.style.textAlign = 'center';
-messageDiv.style.fontSize = '12px';
-messageDiv.style.padding = '2px';
 messageDiv.style.zIndex = '9999';
-messageDiv.style.fontFamily = 'Arial, sans-serif';
-
 // Append the div to the body
 document.body.appendChild(messageDiv);
